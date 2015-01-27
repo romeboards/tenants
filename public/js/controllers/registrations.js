1c16dda222a1a05df145c2c171a3a1fb3128e6b8
Dan Kass <romeboards@Dans-MacBook-Pro-4.local>
2015-01-27 16:56:28 -0500

    separating map into a directive

--- public/js/controllers/registrations.js
+++ public/js/controllers/registrations.js
@@ -14,79 +14,13 @@ angular.module('mean.system').controller('RegController', ['$rootScope','$scope'
       });
 
       Address.getBuildingOwners($scope.addr, function(owners) {
-        $scope.otherbuildings = [];
+        $scope.otherBuildings = [];
         $scope.owners = owners;
       });
 
     }
   });
 
-  $scope.findOtherBuildings = function(owner) {
-    Address.getBuildingsFromBusinessAddr(owner, function(registration) {
-
-      $scope.otherbuildings.push(registration);
-      addMarkerToMap(registration);
-
-    });
-  };
-
-  var geocoder = new google.maps.Geocoder();
-  var bounds = new google.maps.LatLngBounds();
-  var infoWindow = new google.maps.InfoWindow();
-  var circle ={
-    path: google.maps.SymbolPath.CIRCLE,
-    fillColor: 'red',
-    fillOpacity: .8,
-    scale: 4.5,
-    strokeColor: 'white',
-    strokeWeight: 1
-  };
-  var mapOptions = {
-    zoom: 12,
-    center: new google.maps.LatLng(40.714352, -74.005973),
-    mapTypeId: google.maps.MapTypeId.TERRAIN
-  }
-
-  var markers = 0;
-
-  $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-  // $scope.$watch(function(){ return $scope.otherbuildings }, function(val) { 
-  //   console.log('trigger');
-  //   google.maps.event.trigger($scope.map, 'resize');
-  //   $scope.map.setCenter(new google.maps.LatLng(42.3605336, -72.6362989));
-  // });
-
-  var addMarkerToMap = function(registration) {
-
-    var address = registration.housenumber + ' ' + registration.streetname + ' ' + registration.zip;
-
-    Address.getLatLng(address, function(latlng) {
-
-      if(latlng.length) {
-        var lat = latlng[0].latitude,
-            lng = latlng[0].longitude,
-            pos = new google.maps.LatLng(lat, lng);
-
-        $scope.map.setCenter(pos);                     
-
-        var marker = new google.maps.Marker({
-            map: $scope.map,
-            icon: circle,
-            position: pos,
-        });
-
-        marker.content = '<div class="infoWindowContent">' + address + '</div>';
-        
-        google.maps.event.addListener(marker, 'click', function(){
-            infoWindow.setContent(address);
-            infoWindow.open($scope.map, marker);
-        });
-        bounds.extend(marker.position);
-
-        $scope.map.fitBounds(bounds);        
-      }
-    });    
-  };
+  $scope.selectOwner = function(owner) { $scope.selectedOwner = owner; };
 
 }]);
--- public/js/directives.js
+++ public/js/directives.js
@@ -11,3 +11,77 @@ angular.module('mean')
     	};
  	});
 
+angular.module('mean')
+	.directive('registrationsMap', ['Address', function (Address) {
+    return {
+      scope: {
+      	owner: '=',
+        buildings: '='
+      },
+      template: '<div class="map"></div>',
+      link: function (scope, element, attrs) {
+
+        var geocoder = new google.maps.Geocoder();
+        var bounds = new google.maps.LatLngBounds();
+        var infoWindow = new google.maps.InfoWindow();
+        var circle = {
+          path: google.maps.SymbolPath.CIRCLE,
+          fillColor: 'red',
+          fillOpacity: .8,
+          scale: 4.5,
+          strokeColor: 'white',
+          strokeWeight: 1
+        };
+        var mapOptions = {
+          zoom: 12,
+          center: new google.maps.LatLng(40.714352, -74.005973),
+          mapTypeId: google.maps.MapTypeId.TERRAIN
+        }
+
+        var map = new google.maps.Map(element.find('.map')[0], mapOptions);
+
+        var addMarkerToMap = function(registration) {
+
+          var address = registration.housenumber + ' ' + registration.streetname + ' ' + registration.zip;
+
+          Address.getLatLng(address, function(latlng) {
+
+            if(latlng.length) {
+              var lat = latlng[0].latitude,
+                  lng = latlng[0].longitude,
+                  pos = new google.maps.LatLng(lat, lng);
+
+              map.setCenter(pos);                     
+
+              var marker = new google.maps.Marker({
+                  map: map,
+                  icon: circle,
+                  position: pos,
+              });
+
+              marker.content = '<div class="infoWindowContent">' + address + '</div>';
+              
+              google.maps.event.addListener(marker, 'click', function(){
+                  infoWindow.setContent(address);
+                  infoWindow.open(map, marker);
+              });
+              bounds.extend(marker.position);
+
+              map.fitBounds(bounds);        
+            }
+          });    
+        };
+
+        scope.$watch(function(){ return scope.owner }, function(val) {  
+
+          if(scope.owner) {
+            Address.getBuildingsFromBusinessAddr(scope.owner, function(eachRegistration) {      //callback is called for 'each'
+              scope.buildings.push(eachRegistration);
+              addMarkerToMap(eachRegistration);
+            });
+          }
+
+        });
+      }
+  	};
+ 	}]);
